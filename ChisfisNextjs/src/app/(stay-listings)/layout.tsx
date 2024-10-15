61d94e6f27fc01beae32447d253d2995e54f2c08
SaQuita Pace <saquitab@gmail.com>
2024-10-15 17:16:14 -0400

    removed unused database directory

--- ChisfisNextjs/src/app/(stay-listings)/layout.tsx
+++ ChisfisNextjs/src/app/(stay-listings)/layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
       <BgGlassmorphism />
 
       {/* SECTION HERO */}
-      <div className="container pt-10 pb-24 lg:pt-16 lg:pb-28">
+      <div className="container pt-10 pb-24 lg:pt-16 lg:pb-28 hidden">
         <SectionHeroArchivePage currentPage="Stays" currentTab="Stays" />
       </div>
 
