0311e97b688ccd9e32ff0f0d515b3033c1b87efc
SaQuita Pace <saquitab@gmail.com>
2024-11-01 11:49:36 -0400

    login file information for google login

--- ChisfisNextjs/src/app/login/page.tsx
+++ ChisfisNextjs/src/app/login/page.tsx
@@ -11,13 +11,28 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '@/store/store';
 import { loginStart, loginSuccess, loginFailure } from '@/store/slices/LoginSlice';
-
+import facebookSvg from "@/images/Facebook.svg";
+import googleSvg from "@/images/Google.svg";
+import Image from "next/image";
 
 export interface PageLoginProps {}
 export interface LoginFormInputs {
   email: string;
   password: string;
 }
+
+const loginSocials = [
+  {
+    name: "Continue with Facebook",
+    href: "#",
+    icon: facebookSvg,
+  },
+  {
+    name: "Continue with Google",
+    href: "#",
+    icon: googleSvg,
+  },
+];
 
 const PageLogin: FC<PageLoginProps> = ({}) => {
   const router = useRouter();
@@ -99,6 +114,37 @@ const PageLogin: FC<PageLoginProps> = ({}) => {
           Sign In
         </h2>
         <div className="max-w-md mx-auto space-y-6">
+
+        <div className="grid gap-3">
+            {loginSocials.map((item, index) => (
+              <a
+                key={index}
+                href={item.href}
+                className="flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
+              >
+                <Image
+                  className="flex-shrink-0"
+                  src={item.icon}
+                  alt={item.name}
+                />
+                <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
+                  {item.name}
+                </h3>
+              </a>
+            ))}
+            <div>
+        </div>
+          </div>
+          {/* OR */}
+          <div className="relative text-center">
+            <span className="relative z-10 inline-block px-4 font-medium text-sm bg-white dark:text-neutral-400 dark:bg-neutral-900">
+              OR
+            </span>
+            <div className="absolute left-0 w-full top-1/2 transform -translate-y-1/2 border border-neutral-100 dark:border-neutral-800"></div>
+          </div>
+          {/* FORM */}
+
+
         {generalError && <div className="text-red-600 text-sm">{generalError}</div>} {/* General error message */}
           {/* FORM */}
           <form onSubmit={ handleSubmit( handleLogin ) }
